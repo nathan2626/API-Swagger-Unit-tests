4cc37217a4826e899863377660d282ef80ee9730
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2021-05-16 12:22:44 +0200

    Fix bug

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 //Auth
 Route::post('auth/register', [\App\Http\Controllers\ApiTokenController::class, 'register']);
